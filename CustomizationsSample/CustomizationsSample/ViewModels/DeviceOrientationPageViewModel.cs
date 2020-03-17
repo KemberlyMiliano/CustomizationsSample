565f09df2039366feff64d14f8c7a408149357fc
KemberlyMiliano <kemberlymiliano@gmail.com>
2020-03-17 15:55:14 -0400

    Inicial Commit

---
+++ CustomizationsSample/CustomizationsSample/ViewModels/DeviceOrientationPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CustomizationsSample.Models;
+using CustomizationsSample.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace CustomizationsSample.ViewModels
+{
+    public class DeviceOrientationPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public OrientationText Text { get; set; } = new OrientationText();
+        public DeviceOrientationPageViewModel()
+        {
+
+        }
+
+
+    }
+}
